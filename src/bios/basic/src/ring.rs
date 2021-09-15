d5af7079afb234e22938cee4a71ebf4eb711c4fe
M4tsuti <ctsinon@outlook.com>
2021-09-15 21:54:36 +0800

    added enum for privilege levels

---
+++ src/bios/basic/src/ring.rs
@@ -0,0 +1,15 @@
+/// Module for privilege management
+
+/// Privilege level on Intel platform.
+///
+/// - Ring0 is for OS Kernel.
+/// - Ring1 and Ring2 are for OS Services
+/// - Ring3 is for user mode applications
+///
+/// See *Intel Developer Manual Vol. 3A 5-6 5.5 PRIVILEGE LEVELS*
+pub enum Privilege {
+    Ring0 = 0,
+    Ring1 = 1,
+    Ring2 = 2,
+    Ring3 = 3
+}
