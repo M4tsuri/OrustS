39038ba82fdb2f5a654629579dc91d3c7099e156
M4tsuti <ctsinon@outlook.com>
2021-10-11 15:12:18 +0800

    code for creating bootload img optimized

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{fs::File, io::{Read, IoSlice, Write}, path::Path};
+use std::{fs::File, io::{Read, Write}, path::Path};
 use layout::*;
 
 static MINEFEST_PATH: &str = "/Users/ctsinon/Projects/Orangs/bootloader";
-static STAGE1_IMG: &str = "target/stage_1/target/release/stage_1.bin";
-static STAGE2_IMG: &str = "target/stage_2/target/release/stage_2.bin";
-static STAGE3_IMG: &str = "target/stage_3/target/release/stage_3.bin";
+
+static STAGES: [(&str, u32); 3] = [
+    (&"target/stage_1/target/release/stage_1.bin", STAGE1_SIZE),
+    (&"target/stage_2/target/release/stage_2.bin", STAGE2_SIZE),
+    (&"target/stage_3/target/release/stage_3.bin", STAGE3_SIZE)
+];
 
 static TARGET_IMG: &str = "target/bootloader.bin";
 
@@ -12,32 +15,18 @@ fn main() {
     let minefest_path = Path::new(MINEFEST_PATH);
     let target_path = minefest_path.join(TARGET_IMG);
 
-    let stage1_path = minefest_path.join(STAGE1_IMG);
-    let stage2_path = minefest_path.join(STAGE2_IMG);
-    let stage3_path = minefest_path.join(STAGE3_IMG);
-    
-
-    let mut stage1_file = File::open(stage1_path).unwrap();
-    let mut stage2_file = File::open(stage2_path).unwrap();
-    let mut stage3_file = File::open(stage3_path).unwrap();
-
-    let mut stage1_tmp = vec![];
-    let mut stage2_tmp = vec![];
-    let mut stage3_tmp = vec![];
-
-    stage1_file.read_to_end(&mut stage1_tmp).unwrap();
-    stage2_file.read_to_end(&mut stage2_tmp).unwrap();
-    stage3_file.read_to_end(&mut stage3_tmp).unwrap();
-
-    stage1_tmp.resize(STAGE1_SIZE as usize, 0);
-    stage2_tmp.resize(STAGE2_SIZE as usize, 0);
-    stage3_tmp.resize(STAGE3_SIZE as usize, 0);
-
-    let stage1 = IoSlice::new(stage1_tmp.as_slice());
-    let stage2 = IoSlice::new(stage2_tmp.as_slice());
-    let stage3 = IoSlice::new(stage3_tmp.as_slice());
+    let extract_stage = |stage_dir: &str, size: u32| {
+        let stage_path = minefest_path.join(stage_dir);
+        let mut stage_file = File::open(stage_path).unwrap();
+        let mut tmp = vec![];
+        stage_file.read_to_end(&mut tmp).unwrap();
+        tmp.resize(size as usize, 0);
+        tmp
+    };
 
     let mut res = File::create(target_path).unwrap();
-    res.write_vectored(&[stage1, stage2, stage3]).unwrap();
+    for (dir, size) in STAGES {
+        res.write(&extract_stage(dir, size)).unwrap();
+    }
     res.flush().unwrap();
 }
