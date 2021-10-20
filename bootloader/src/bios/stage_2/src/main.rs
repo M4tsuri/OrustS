fd1bf0dd86bf145308b281c270e86be8f1b9de8a
M4tsuti <ctsinon@outlook.com>
2021-10-20 21:13:01 +0800

    [Opt] stage 2 code refactored to deal with exceptions

--- bootloader/src/bios/stage_2/src/main.rs
+++ bootloader/src/bios/stage_2/src/main.rs
@@ -18,17 +18,23 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn main() -> Result<(), &'static str> {
+    display_real("Stage 2 entered.");
+    load_stage3()?;
+    display_real("Stage 3 loaded.");
+    to_protect()?;
+    Ok(())
+}
+
 /// Our entrypoiny of bootloader.
 /// The loader will be loaded to 0x7c00 by BIOS, which has been considered by our linker script
 #[link_section = ".startup"]
 #[no_mangle]
 fn _start() -> ! {
-    display_real("Stage 2 entered.");
-    if let Err(msg) = load_stage3() {
+    if let Err(msg) = main() {
         display_real(msg);
+        unsafe { asm!("hlt;") };
     }
-    display_real("Stage 3 loaded.");
-    to_protect();
     
     let stage_3: fn() -> ! = unsafe { transmute(&STAGE3_PTR as *const PhantomData<()>) };
     stage_3()
