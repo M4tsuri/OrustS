37370ccd368c52d00e8d81f792bd20a205531df7
M4tsuti <ctsinon@outlook.com>
2021-10-21 17:05:33 +0800

    [Feature] implemented a simple kernel build system

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[used]
+static TMP: [u8; 0x100] = [0; 0x100];
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
 fn main() {
-    println!("Hello, world!");
+    let a = TMP[3];
+    let b = TMP[4];
+    if a * a + b * b == a * b {
+        let c = Some(a);   
+    }
+    loop {}
 }
