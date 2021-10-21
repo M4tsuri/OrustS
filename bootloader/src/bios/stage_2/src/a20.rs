2446cb4fa8e4ec22d23584f62ac2908eb77e3a6a
M4tsuti <ctsinon@outlook.com>
2021-10-21 20:44:46 +0800

    [Doc] added some docs about checking a20 line

--- bootloader/src/bios/stage_2/src/a20.rs
+++ bootloader/src/bios/stage_2/src/a20.rs
@@ -1,6 +1,11 @@
 use core::ptr::addr_of;
 
-/// Code for checking and enabling the A20 line
+/// Code for checking and enabling the A20 line.
+/// If the A20 line of a CPU is not enabled, the max address it can use is 0xfffff
+/// due to hardware limitation. All memory access with address larger than it will be 
+/// wrapped around.
+/// For example, the address 0xf800:0x8000 will be wrapped around to 0x0.
+/// So we can check whether A20 line is enabled with this interesting mechanism.
 pub fn check_a20() -> bool {
     let mut magic: usize = 0xdeadbeef;
     let magic_ptr = addr_of!(magic) as usize;
